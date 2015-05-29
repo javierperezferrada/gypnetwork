2018815a09250c7cfdd1072775af061638a7faa3
Javier <javier@javier-Lenovo-G480.(none)>
2015-05-29 15:27:43 -0400

    detalles solucionados y formulario implementado

--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
-from wtforms import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
+from flask_wtf import Form
+from wtforms import  TextField, TextAreaField, SubmitField, validators, ValidationError
  
 class ContactForm(Form):
-  name = TextField("Name",  [validators.Required("Please enter your name.")])
-  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
-  subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
-  message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
-  submit = SubmitField("Send")
+  nombre = TextField("Nombre")
+  email = TextField("Email")
+  telefono = TextField("Telefono")
+  message = TextAreaField("Mensage")
+  submit = SubmitField("Enviar")
--- routes.py
+++ routes.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
-
+from flask.ext.mail import Message, Mail
+ 
+mail = Mail()
+ 
 app = Flask(__name__)
  
 app.secret_key = 'development key'
  
-
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'gypnetwork.info@gmail.com'
+app.config["MAIL_PASSWORD"] = 'charchatel'
+ 
+mail.init_app(app)
  
 @app.route('/')
 @app.route('/inicio')
@@ -24,23 +33,25 @@ def ubicacion():
 def galeria():
   return render_template('galeria.html')
 
-@app.route('/contacto')
-def contacto():
-  return render_template('contacto.html')
 
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
+
+@app.route('/contacto', methods=['GET', 'POST'])
+def contacto():
   form = ContactForm()
  
   if request.method == 'POST':
-    if form.validate() == False:
-      flash('All fields are required.')
-      return render_template('contact.html', form=form)
-    else:
-      return 'Form posted.'
+
+      msg = Message('Mensaje enviado a traves de www.gypnetwork.cl', sender=form.email.data, recipients=['gypnetwork.info@gmail.com'])
+      msg.body = """\n
+      Nombre: %s \ne-mail:  %s \nTelefono: %s \nMensaje: 
+      %s
+      """ % (form.nombre.data, form.email.data, form.telefono.data, form.message.data)
+      mail.send(msg)
+ 
+      return render_template('contacto.html', success=True)
  
   elif request.method == 'GET':
-    return render_template('contact.html', form=form)
+    return render_template('contacto.html', form=form)
  
 if __name__ == '__main__':
   app.run(debug=True)
